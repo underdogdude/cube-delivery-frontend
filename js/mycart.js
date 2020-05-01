df61722ad705e0571dc1e1685b2cb235ba798cfb
underdogdude <underdogbright@gmail.com>
2020-05-01 22:39:35 +0800

    ADD add and remove amount in cart

--- js/mycart.js
+++ js/mycart.js
@@ -4,23 +4,75 @@ var myCart = {
         var get_amount = 0;
         var get_total = 0;
 
-        for (var i in cartItems) {
-            get_total += cartItems[i].totalPrice * cartItems[i].qty;
-            get_amount += cartItems[i].qty;
+        if(Object.keys(cartItems).length > 0) { 
+            for (var i in cartItems) {
+                get_total += cartItems[i].totalPrice * cartItems[i].qty;
+                get_amount += cartItems[i].qty;
+            }
+    
+            $("#mycart_total").html(get_total.toLocaleString());
+            $("#mycart_amount").html(get_amount);
+            $("#mycart__btn_container").css("display", "block");
+        }else { 
+            $("#mycart__btn_container").css("display", "none");
+            window.location = "./index.html";
         }
-
-        $("#mycart_total").html(get_total.toLocaleString());
-        $("#mycart_amount").html(get_amount);
-        $("#mycart__btn_container").css("display", "block");
-    },
-    clicked: function () {
-        var element = document.querySelector("main");
-        element.classList.add("animated", "slideOutLeft");
-        element.addEventListener("animationend", function () {
-            window.location = "./mycart.html";
-        });
-    },
+    }
 };
+
+
+var amount_btn = { 
+
+    increase: function(key) { 
+
+        var amount = this.getCurrentAmount(key);
+        let $input = $("#" + key + "-quatityInput"),
+            val = parseInt($input.val()),
+            max = parseInt($input.attr('max')),
+            step = parseInt($input.attr('step'));
+
+        let temp = val + step;
+        if (temp <= max) { 
+            $input.val(temp);
+            amount = temp;
+        }else{
+            $input.val(max);
+            amount = max;
+        }
+        this.changeAmount(key, amount);
+    },
+    decrease: function(key) { 
+
+        var amount = this.getCurrentAmount(key);
+        let $input = $("#" + key + "-quatityInput"),
+            val = parseInt($input.val()),
+            min = parseInt($input.attr('min')),
+            step = parseInt($input.attr('step'));
+
+        let temp = val - step;
+        if (temp >= min) { 
+            $input.val(temp);
+            amount = temp;
+        }else{
+            $input.val(min);
+            amount = min;
+        }
+        this.changeAmount(key, amount);
+    },
+
+    getCurrentAmount: function(key) { 
+        return $("#" + key + "-quatityInput").val();
+    },
+
+    changeAmount: function(id, amount){
+
+        var cartInLocal = cartFunction.getItem();
+        cartInLocal.items[id].qty = Number(amount);
+        localStorage.setItem("cart", JSON.stringify(cartInLocal));
+        reRender();
+    }
+}
+
 
 
 var cartLists = {
@@ -54,17 +106,19 @@ var cartLists = {
                         บาท
                     </span>
                     <div class="custom-input-checkout">
-                        <button type="button" class="cic-btn cic-btn-1 cic-btn-small cic-decrement">
+                        <button type="button" class="cic-btn cic-btn-1 cic-btn-small cic-decrement"
+                            onclick="amount_btn.decrease('${i}')">
                             <i class="fas fa-minus"></i>
                         </button>
                         <input type="number" 
-                            class="cic-input" id="quatityInput"
+                            class="cic-input" id="${i}-quatityInput"
                             step="1" 
                             value="${items[i].qty}" 
-                            min="0"
+                            min="1"
                             max="99"
                         />
-                        <button type="button" class="cic-btn cic-btn-1 cic-btn-small cic-increment">
+                        <button type="button" class="cic-btn cic-btn-1 cic-btn-small cic-increment"
+                            onclick="amount_btn.increase('${i}')">
                             <i class="fas fa-plus"></i>
                         </button>
                     </div>
@@ -110,4 +164,196 @@ function reRender() {
     var cartInLocal = cartFunction.getItem();
     myCart.showBtn();
     cartLists.render(cartInLocal.items);
+}
+
+
+function checkout() { 
+
+    axios({
+            method: 'post',
+            url: 'https://asia-east2-cube-family-delivery-dev.cloudfunctions.net/api/slack/sendMessage',
+            data: {
+                "channel": window.slack_channelId,
+                "blocks": 
+                        [
+                            {
+                                "type": "context",
+                                "elements": [
+                                    {
+                                        "text": "*November 12, 2019*  |  Ordering Team Announcements",
+                                        "type": "mrkdwn"
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "divider"
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "*Customer Detail*"
+                                }
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": `\n\n *Name*, ${window.customer_displayName} \n\n *Phone*, 0838692401 \n\n *Address*, 1004/142 Nirun Residence 9, floor 6, building C,  Sukhumwit Road, Bang Na, Bang Na, Bangkok 10260`
+                                },
+                                "accessory": {
+                                    "type": "image",
+                                    "image_url": `${window.customer_avatar}`,
+                                    "alt_text": `${window.customer_displayName} avatar`
+                                }
+                            },
+                            {
+                                "type": "divider"
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": ":calendar: |   *COMING ORDER*  | :calendar: "
+                                }
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "*Buy 3 Get 1, ROOIBOS MILK TEA*"
+                                },
+                                "accessory": {
+                                    "type": "button",
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "x1",
+                                        "emoji": true
+                                    }
+                                }
+                            },
+                            {
+                                "type": "context",
+                                "elements": [
+                                    {
+                                        "type": "mrkdwn",
+                                        "text": "*รายการที่ 1*: \n\n • ใส่แก้ว หวานธรรมชาติ \n• ใส่แก้ว เพิ่มน้ำตาลช่อดอกมะพร้าว 3กรัม"
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "context",
+                                "elements": [
+                                    {
+                                        "type": "mrkdwn",
+                                        "text": "*รายการที่ 2*: \n\n • ใส่แก้ว หวานธรรมชาติ \n• ใส่แก้ว เพิ่มน้ำตาลช่อดอกมะพร้าว 3กรัม"
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "divider"
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "*Rooibos - LATTE ( ชารอยบอสลาเต้ )*"
+                                },
+                                "accessory": {
+                                    "type": "button",
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "x3",
+                                        "emoji": true
+                                    }
+                                }
+                            },
+                            {
+                                "type": "context",
+                                "elements": [
+                                    {
+                                        "type": "mrkdwn",
+                                        "text": "*Topping*: \n\n •  Rooibos - Bubble"
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "context",
+                                "elements": [
+                                    {
+                                        "type": "mrkdwn",
+                                        "text": "*package*: \n\n • ใส่แก้ว"
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "*Rooibos - LATTE ( ชารอยบอสลาเต้ )*"
+                                },
+                                "accessory": {
+                                    "type": "button",
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "x1",
+                                        "emoji": true
+                                    }
+                                }
+                            },
+                            {
+                                "type": "context",
+                                "elements": [
+                                    {
+                                        "type": "mrkdwn",
+                                        "text": "*package*: \n\n • ใส่ขวด"
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "divider"
+                            },
+                            {
+                                "type": "context",
+                                "elements": [
+                                    {
+                                        "type": "mrkdwn",
+                                        "text": "\n\n`Total Price : 1000 Baht`\n\n"
+                                    }
+                                ]
+                            },
+                                {
+                                "type": "actions",
+                                "elements": [
+                                    {
+                                        "type": "button",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "emoji": true,
+                                            "text": "Approve"
+                                        },
+                                        "style": "primary",
+                                        "value": "click_me_123"
+                                    },
+                                    {
+                                        "type": "button",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "emoji": true,
+                                            "text": "Deny"
+                                        },
+                                        "style": "danger",
+                                        "value": "click_me_123"
+                                    }
+                                ]
+                            }
+                        ]
+            },
+            headers: {
+                'Content-Type': 'application/json'
+            }
+    })
+    .then(function (response) {
+        console.log(response);
+    })
 }
